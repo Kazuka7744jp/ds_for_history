bfd6e6cdab969401d02eeebaf9b9cc29042b16d4
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-20 23:41:44 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+get_ipython().system('pip install streamlit')
+
+
+# In[2]:
+
 
 import matplotlib.pyplot as plt
 import japanize_matplotlib
@@ -10,18 +18,24 @@ import pandas as pd
 import streamlit as st
 
 
-# In[8]:
+# In[3]:
 
 
 df1 = pd.read_pickle("data/taishi.pkl")
 df2 = pd.read_pickle("data/bb.pkl")
 
 
-# ## ４．ヒストグラム・ローレンツ曲線
+# In[20]:
 
-# ## プロ野球選手の年俸のヒストグラム
 
-# In[10]:
+markdown = """
+### プロ野球選手の年俸のヒストグラム
+2020年のプロ野球選手の年俸からヒストグラムを作成しました
+"""
+st.write(markdown)
+
+
+# In[4]:
 
 
 # https://pystyle.info/statistics-frequency-table-and-histogram/
@@ -31,54 +45,55 @@ plt.xlabel("年収")
 plt.ylabel("人数")
 plt.hist(df2["年俸"],alpha=0.5, bins=50)
 plt.show()
+st.pyplot(fig)
 
 
-# In[11]:
+# In[5]:
 
 
 df2 = df2[df2["年俸"]!="#VALUE!"]
 df2 = df2.reset_index(drop=True)
 
 
-# In[12]:
+# In[6]:
 
 
 df2["年俸"] = df2["年俸"].apply(lambda x:x.replace(",",""))
 
 
-# In[13]:
+# In[7]:
 
 
 df2["年俸"] = df2["年俸"].astype('int')
 
 
-# In[14]:
+# In[8]:
 
 
 df2["年俸"] = df2["年俸"].apply(lambda x: x/10000)
 
 
-# In[15]:
+# In[9]:
 
 
 bins = np.linspace(0, df2["年俸"].max(), 91)
 print(bins)
 
 
-# In[16]:
+# In[10]:
 
 
 freq = df2["年俸"].value_counts(bins=bins, sort=False)
 freq
 
 
-# In[17]:
+# In[11]:
 
 
 bins[1:]
 
 
-# In[18]:
+# In[12]:
 
 
 class_value = (bins[:-1] + bins[1:]) / 2  # 階級値
@@ -87,13 +102,13 @@ cum_freq = freq.cumsum()  # 累積度数
 rel_cum_freq = rel_freq.cumsum()  # 相対累積度数
 
 
-# In[19]:
+# In[13]:
 
 
 pd.set_option('display.max_rows', 101)
 
 
-# In[20]:
+# In[14]:
 
 
 dist = pd.DataFrame(
@@ -119,17 +134,18 @@ ax2 = ax1.twinx()
 ax2.plot(np.arange(len(dist)), dist["相対累積度数"], "--o", color="k")
 ax2.set_ylabel("累積相対度数")
 ax2.legend()
+st.pyplot(fig)
 
 
 # ## ローレンツ曲線
 
-# In[24]:
+# In[16]:
 
 
 df2["年俸"].mean()
 
 
-# In[25]:
+# In[17]:
 
 
 df2["年俸"].mode()
@@ -137,7 +153,7 @@ df2["年俸"].mode()
 
 # ## 共分散
 
-# In[37]:
+# In[18]:
 
 
 df1.cov()
