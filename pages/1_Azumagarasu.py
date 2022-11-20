661cc38f79ab7038dda105471113222b926d7f52
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-21 02:50:55 +0900

    Add files via upload

--- pages/1_Azumagarasu.py
+++ pages/1_Azumagarasu.py
@@ -10,11 +10,40 @@ import streamlit as st
 # In[2]:
 
 
-st.header('Under Construction')
+st.header('残された「点帖」')
+
+
+# In[3]:
+
+
+st.write('点帖とは句会の記録であり、執筆が書いたものを点者、宗匠が批点・押印、最終的には、秀逸の物の褒美として贈られるものである')
+st.write('家にこの点帖が残されていたということは、この「玄」が当家にまつわる人間である可能性が高い')
+
+
+# In[9]:
+
+
+st.header("点帖1")
+st.image("../pic/daikaku1.jpg")
+
+
+# In[10]:
+
+
+st.header("点帖2")
+st.image("../pic/daikaku2.jpg")
+
+
+# In[11]:
+
+
+st.header("点帖3")
+st.image("../pic/daikaku3.jpg")
 
 
 # In[ ]:
 
 
-
+st.header("点帖3")
+st.image("../pic/daikaku3.jpg")
 
