e6b507ff1da4f64a34bbca19fc9e4fe2990bdede
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-21 01:34:39 +0900

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+
+# In[ ]:
+
+
+st.title("東烏")
+st.header('「鬼神をも涙する」俳諧と生きた男')
+st.write('このページでは、江戸時代の刈谷の油屋「九右衛門（俳号「東烏」）」が残した和歌に関する日記「和歌芽籠」を読み解きながら、江戸時代の三河俳諧を調査するページです。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
+
+
+# In[ ]:
+
+
+st.write("主なページ一覧")
+st.write('「Azumagarasu」東烏に関する調査ページ')
+st.write('「Nobunaga」信長の野望のデータセットを利用したデータ可視化練習')
+st.write('「BaseBall」2020年のプロ野球選手データを利用したデータ可視化練習')
+
