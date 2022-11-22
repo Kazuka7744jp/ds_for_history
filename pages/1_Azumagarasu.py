577ddd3e060559a9edd25bcfaa0aba329967abd9
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-22 13:08:20 +0900

    Update 1_Azumagarasu.py

--- pages/1_Azumagarasu.py
+++ pages/1_Azumagarasu.py
@@ -6,44 +6,25 @@
 
 import streamlit as st
 
+st.title(東烏に関する調査ページ)
 
-# In[2]:
+st.set_page_config(layout="wide")
+#セレクトボックスのリストを作成
+pagelist = ["はじめに","残された点帖"]
+#サイドバーのセレクトボックスを配置
+selector=st.sidebar.selectbox( "ページ選択", pagelist)
+if selector=="はじめに":
+  st.header('はじめに')
 
-
-st.header('残された「点帖」')
-
-
-# In[3]:
-
-
-st.write('点帖とは句会の記録であり、「執筆」が書いたものを「点者」「宗匠」が批点・押印、最終的には、秀逸の者の褒美として贈られるものである')
-st.write('家にこの点帖が残されていたということは、俳号「玄」が当家にまつわる人間である可能性が高い。')
-
-
-# In[9]:
-
-
-st.header("点帖1")
-st.image("pic/daikaku1.jpg")
-
-
-# In[10]:
-
-
-st.header("点帖2")
-st.image("pic/daikaku2.jpg")
-
-
-# In[11]:
-
-
-st.header("点帖3")
-st.image("pic/daikaku3.jpg")
-
-
-# In[ ]:
-
-
-st.header("点帖4")
-st.image("pic/daikaku4.jpg")
-
+elif selector=="page2":
+  st.header("残された点帖")
+  st.write('点帖とは句会の記録であり、「執筆」が書いたものを「点者」「宗匠」が批点・押印、最終的には、秀逸の者の褒美として贈られるものである')
+  st.write('家にこの点帖が残されていたということは、俳号「玄」が当家にまつわる人間である可能性が高い。')
+  st.header("点帖1")
+  st.image("pic/daikaku1.jpg")
+  st.header("点帖2")
+  st.image("pic/daikaku2.jpg")
+  st.header("点帖3")
+  st.image("pic/daikaku3.jpg")
+  st.header("点帖4")
+  st.image("pic/daikaku4.jpg")
