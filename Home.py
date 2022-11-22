881e199afb8dd2f6e979a3fedfe0f089d3d6f40c
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-22 14:53:05 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@
 
 import streamlit as st
 
-st.set_page_config(page_title="東烏", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="東烏", page_icon=None, layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
 st.title("東烏")
 st.header('「鬼神をも涙する」俳諧と生きた男')
