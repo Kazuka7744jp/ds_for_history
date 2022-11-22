d851927f5bbbfc0df6c89a83978bb3f0f1748eb4
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-22 19:03:04 +0900

    Update 2_Nobunaga.py

--- pages/2_Nobunaga.py
+++ pages/2_Nobunaga.py
@@ -9,7 +9,7 @@ import folium
 
 m = folium.Map(
     # 地図の中心位置の指定(今回は栃木県の県庁所在地を指定)
-    location=[36.56583, 139.88361], 
+    location=[35.02968298040031, 137.0168353420444], 
     # タイル、アトリビュートの指定
     tiles='https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
     attr='都道府県庁所在地、人口、面積(2016年)',
