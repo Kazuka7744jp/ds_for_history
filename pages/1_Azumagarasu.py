95d1f32ba5bcb0af5e013d3a117e4da7d99bb1d5
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-22 13:33:05 +0900

    Update 1_Azumagarasu.py

--- pages/1_Azumagarasu.py
+++ pages/1_Azumagarasu.py
@@ -16,7 +16,7 @@ selector=st.sidebar.selectbox( "ページ選択", pagelist)
 if selector=="はじめに":
   st.header('はじめに')
 
-elif selector=="page2":
+elif selector=="残された点帖":
   st.header("残された点帖")
   st.write('点帖とは句会の記録であり、「執筆」が書いたものを「点者」「宗匠」が批点・押印、最終的には、秀逸の者の褒美として贈られるものである')
   st.write('家にこの点帖が残されていたということは、俳号「玄」が当家にまつわる人間である可能性が高い。')
