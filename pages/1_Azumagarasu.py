c01fa84aaba977e234d7def71a11af1b277eb0bf
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-23 12:18:44 +0900

    Update 1_Azumagarasu.py

--- pages/1_Azumagarasu.py
+++ pages/1_Azumagarasu.py
@@ -15,6 +15,9 @@ pagelist = ["はじめに","残された点帖"]
 selector=st.sidebar.selectbox( "ページ選択", pagelist)
 if selector=="はじめに":
   st.header('はじめに')
+  st.write('このページは、愛知県刈谷市の東海道沿いの家に残された古文書などに基づく、先祖の在りし日の姿の調査記録になります。')
+  st.write('調査者：')
+  
 
 elif selector=="残された点帖":
   st.header("残された点帖")
