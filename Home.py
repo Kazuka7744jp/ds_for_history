53717a64f3186e45ae9a6f6ae894024d5d2b9c58
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-25 00:49:20 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,8 +7,9 @@ st.set_page_config(page_title="東烏", page_icon="pic/karasu.jpg", layout="wide
 
 st.title("東烏")
 st.subheader(" 「鬼神をも涙する」俳諧と生きた男")
-st.image("pic/wakamegomo.jpg")
-st.write('このページでは、江戸時代の刈谷の油屋「久右衛門（?~1803年?　俳号「東烏」)が残した和歌に関する日記「和歌芽籠」を読み解きながら、江戸時代の三河俳諧を調査するページです。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
+left_column1, right_column1 = st.columns(2)
+left_column1.image("pic/wakamegomo.jpg")
+right_column1 .write('このページでは、江戸時代の刈谷の油屋「久右衛門（?~1803年?　俳号「東烏」)が残した和歌に関する日記「和歌芽籠」を読み解きながら、江戸時代の三河俳諧を調査するページです。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
 
 st.write("■ページ一覧（左のサイドバーから選択してください）")
 st.write('「Azumagarasu」東烏に関する調査ページ')
