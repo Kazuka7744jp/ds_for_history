360ed20fb0fd3000f6b9ca459c5dded58d8d6ca0
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-25 00:50:30 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,8 +11,8 @@ left_column1, right_column1 = st.columns(2)
 left_column1.image("pic/wakamegomo.jpg")
 right_column1 .write('このページでは、江戸時代の刈谷の油屋「久右衛門（?~1803年?　俳号「東烏」)が残した和歌に関する日記「和歌芽籠」を読み解きながら、江戸時代の三河俳諧を調査するページです。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
 
-st.write("■ページ一覧（左のサイドバーから選択してください）")
-st.write('「Azumagarasu」東烏に関する調査ページ')
-st.write('「Nobunaga」信長の野望のデータセットを利用したデータ可視化練習')
-st.write('「BaseBall」2020年のプロ野球選手データを利用したデータ可視化練習')
+right_column1.write("■ページ一覧（左のサイドバーから選択してください）")
+right_column1.write('「Azumagarasu」東烏に関する調査ページ')
+right_column1.write('「Nobunaga」信長の野望のデータセットを利用したデータ可視化練習')
+right_column1.write('「BaseBall」2020年のプロ野球選手データを利用したデータ可視化練習')
 
