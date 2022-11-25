ad45098b48127c92d002df941223f1f7c49cfa51
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-11-25 12:56:10 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(page_title="東烏", page_icon="pic/karasu.jpg", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 st.title("東烏")
-st.subheader(" 「鬼神にもあわれと思わせる俳諧と生きた男の一生」")
+st.subheader(" 「鬼神にもあわれと思わせる俳諧と生きた男の生きざま」")
 left_column1, right_column1 = st.columns(2)
 left_column1.image("pic/wakamegomo.jpg")
 right_column1 .write('このページでは、江戸時代の刈谷の油屋「久右衛門（?~1803年?　俳号「東烏」)が残した和歌に関する日記「和歌芽籠」を読み解きながら、江戸時代の三河俳諧を調査するページです。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
