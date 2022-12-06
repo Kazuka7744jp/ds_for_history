44f275b547556c1ccc5e2bee9895b232aba083ce
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-12-06 16:42:51 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,6 +7,17 @@ import streamlit as st
 from time import time
 import os
 
+st.set_page_config(page_title="東烏", page_icon="pic/karasu.jpg", layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.subheader(" 「鬼神もあわれむ俳諧と生きた男」")
+st.image("pic/logo.jpg")
+st.write('このサイトでは、江戸時代の刈谷の油屋「加塚久右衛門（かづか きゅうえもん　?~1803年?　俳号「東烏」)が残した和歌に\
+関する日記「和歌芽籠」を読み解きながら、久右衛門の人生や、それから派生した江戸時代の三河俳諧の調査結果をまとめています。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
+
+st.write('''**■ページ一覧（左のサイドバーから選択してください）**''')
+st.write('''**「Azumagarasu」東烏に関する調査ページ**''')
+st.write('「Nobunaga」信長の野望のデータセットを利用したデータ可視化練習')
+st.write('「BaseBall」2020年のプロ野球選手データを利用したデータ可視化練習')
+
 load_dotenv(".env")
 
 DETA_KEY = os.getenv("DETA_KEY")
@@ -34,17 +45,6 @@ st.write("閲覧人数：", len(poets))
 # area = "三河"
 # occupation = "油屋"
 # detail = "刈谷の今川で油屋を営む。「和歌芽籠」を執筆。"
-
-st.set_page_config(page_title="東烏", page_icon="pic/karasu.jpg", layout="wide", initial_sidebar_state="auto", menu_items=None)
-st.subheader(" 「鬼神もあわれむ俳諧と生きた男」")
-st.image("pic/logo.jpg")
-st.write('このサイトでは、江戸時代の刈谷の油屋「加塚久右衛門（かづか きゅうえもん　?~1803年?　俳号「東烏」)が残した和歌に\
-関する日記「和歌芽籠」を読み解きながら、久右衛門の人生や、それから派生した江戸時代の三河俳諧の調査結果をまとめています。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
-
-st.write('''**■ページ一覧（左のサイドバーから選択してください）**''')
-st.write('''**「Azumagarasu」東烏に関する調査ページ**''')
-st.write('「Nobunaga」信長の野望のデータセットを利用したデータ可視化練習')
-st.write('「BaseBall」2020年のプロ野球選手データを利用したデータ可視化練習')
 
 # db = deta.Base("azumagarasu")
 
