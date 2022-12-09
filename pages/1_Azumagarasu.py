7566d3333bfb196660469d2e23e64fd68124b8a1
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-12-10 05:49:43 +0900

    Update 1_Azumagarasu.py

--- pages/1_Azumagarasu.py
+++ pages/1_Azumagarasu.py
@@ -84,7 +84,7 @@ elif selector=="残された点帖":
 elif selector=="三河俳人検索DB":
   st.header("三河俳人検索DB")
   df_haijin = pd.read_csv("data/haijin.csv", header=[0,1], keep_default_na=False)
-  df_kusyu = pd.read_csv("data/kusyu.csv",dtype={'No.': 'int'})
+  df_kusyu = pd.read_csv("data/kusyu.csv",dtype={"No.": "int"})
 
   st.write(df_haijin)
   st.write(df_kusyu)
