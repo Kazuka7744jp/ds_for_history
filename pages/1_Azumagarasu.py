a3c54201433c14d498e0c23314d59a5b3ca4c371
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-12-17 15:10:40 +0900

    Update 1_Azumagarasu.py

--- pages/1_Azumagarasu.py
+++ pages/1_Azumagarasu.py
@@ -134,7 +134,6 @@ elif selector=="三河俳人検索DB":
         axes[idx//2,idx%2].set_title(f"{master}の門下生の職業（n={df_num}）")
         axes[idx//2,idx%2].pie(df_temp.values.flatten(), labels=df_temp.index, autopct='%1.1f%%')
         idx += 1
-    plt.show()
     return fig
     
   st.write(monka_job_pie(df_haijin, "門下", "職業"))
