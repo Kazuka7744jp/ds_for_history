7c9646485003b33dab5ad7979b07d6ba319905ba
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-12-18 13:37:02 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,11 +1,4 @@
-# !/usr/bin/env python
-# coding: utf-8
-# import datetime
-# from deta import Deta
-# from dotenv import load_dotenv
 import streamlit as st
-# from time import time
-# import os
 import pandas as pd
 import plotly.express as px
 
@@ -32,7 +25,6 @@ st.write('■データベース一覧')
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("登録人数", df_len)
 col2.metric("調査済句集", len(df_haijin.columns)-9)
-#   col3, col4 = st.columns(2)
 col3.metric("本名判明率", "{:.1%}".format((df_haijin["本名/別名"]!="").sum()/df_len))
 col4.metric("職業判明率", "{:.1%}".format((df_haijin["職業_詳細"]!="").sum()/df_len))
 st.write(df_haijin)
@@ -52,40 +44,3 @@ st.plotly_chart(job_pie)
 
 st.write("■門下別の職業の割合")
 st.image("pic/monka.png")
-# st.subheader(" 「鬼神もあわれむ俳諧と生きた男」")
-# st.image("pic/logo.jpg")
-# st.markdown("---")
-# st.write(''':seedling: **About this website** :seedling:''')
-# st.write('このサイトでは、江戸時代の刈谷の油屋「加塚久右衛門（かづか きゅうえもん　?~1803年?　俳号「東烏」)が残した和歌に\
-# 関する日記「和歌芽籠」を読み解きながら、久右衛門の人生や、それから派生した江戸時代の三河俳諧の調査結果をまとめています。あわせて管理人のPythonによるデータ可視化の練習結果も掲載しています。')
-
-# st.write(''':seedling: **ページ一覧** :seedling:''')
-# st.write("各ページは、左側のサイドバーから選択してください")
-# st.write(''':one:**「Azumagarasu」東烏に関する調査ページ**''')
-# st.write(':two:「Nobunaga」信長の野望のデータセットを利用したデータ可視化練習')
-# # st.write(':three:「BaseBall」2020年のプロ野球選手データを利用したデータ可視化練習')
-
-# # load_dotenv(".env")
-# # DETA_KEY = os.getenv("DETA_KEY")
-# # deta = Deta(DETA_KEY)
-
-# # # Deta上のデータベースに接続
-# # db = deta.Base("view_count")
-
-# # @st.cache
-# # def insert_view(_time):
-# #     return db.put({"閲覧日時": _time})
-
-# # @st.cache
-# # def fetch_all_poets():
-# #     res = db.fetch()
-# #     return res.items
-
-# # insert_view(str(datetime.datetime.now()))
-
-# # data = fetch_all_poets()
-# # views = pd.DataFrame(data)
-
-# # st.markdown("---")
-# # # カウンターを表示
-# # st.write(f"これまで、:star:累計{len(views)}人:star:の方に、東烏の生き方に触れていただきました。ありがとうございます:bamboo:")
