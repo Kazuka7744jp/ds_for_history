5cd575d3637c25220479765b108fbae57cea6fdf
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2022-12-18 13:27:12 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ import streamlit as st
 # from time import time
 # import os
 import pandas as pd
-
+import plotly.express as px
 
 st.set_page_config(page_title="東烏", page_icon="pic/karasu.jpg", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
