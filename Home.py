e3e8d29cc60ea33551258dc301b9e3efe28a65f3
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2023-01-28 12:00:21 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -45,6 +45,29 @@ AgGrid(df_haijin)# st.write('■職業別の人数の割合')
 #    hover_name=df_job.index)
 # st.plotly_chart(job_pie)
 
+gb = GridOptionsBuilder.from_dataframe(df_haijin)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    df_haijin,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+)
+
+data = grid_response['data']
+selected = grid_response['selected_rows'] 
+df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+
 st.write("■門下・派閥の人数")
 st.image("pic/deshi.png")
 
