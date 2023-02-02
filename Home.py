c9a879bbde89ba3852e2ea40b7e29a0946d2e404
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2023-02-02 12:52:26 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -53,10 +53,9 @@ st.subheader("検索条件を入力または選択してください")
 # haijin_input = st.text_input("俳名または本名・別名")
 # location_input = st.text_input("出身地")
 # job_input = st.text_input("職業")
-haijin_input = st.selectbox("俳名または本名・別名", [""] + sorted(list(df_haijin['俳名'].unique() + sorted(list(df_haijin['本名/別名'].unique())))
+haijin_input = st.selectbox("俳名または本名・別名", [""] + sorted(list(df_haijin['俳名'].unique()) + sorted(list(df_haijin['本名/別名'].unique())))
 location_input = st.selectbox("出身地", [""] + sorted(list(df_haijin['出身地'].unique())))
 job_input = st.selectbox("職業", [""] + sorted(list(df_haijin['職業_詳細'].unique())))
-
 
 if haijin_input or location_input or job_input:
     conditions = (df_haijin['俳名'].str.contains(haijin_input) | df_haijin['本名/別名'].str.contains(haijin_input)) &(df_haijin['出身地'].str.contains(location_input)) &(df_haijin['職業_詳細'].str.contains(job_input))
