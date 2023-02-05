3b630a8a428d14ffbc05dbeb00fb4d9fdfb9c365
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2023-02-05 10:27:14 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -64,19 +64,36 @@ if haijin_input or location_input or job_input:
     st.write("■検索結果一覧")
     st.dataframe(df_selected)
     st.write("■検索結果個人カード　※黄色い英文字が出る場合は読み込み中です")
-                              
+    
+    cards = []
     for index, person in df_selected.iterrows():
-      card(
-          title=person["俳名"],
-          text="【本名/別名】 " + person["本名/別名"] +
-          "\n\n【出身地】 " + person["出身地"] +
-          "\n\n【職業】 " + person["職業_詳細"] +
-          "\n\n【句集登場回数】" + person["句集登場回数"] + "回" +
-          "\n\n【備考】 " + person["備考"],
-          image= person["URL"],
-          url=""
+        cards.append(
+            card(
+                title=person["俳名"],
+                text="【本名/別名】 " + person["本名/別名"] +
+                "\n\n【出身地】 " + person["出身地"] +
+                "\n\n【職業】 " + person["職業_詳細"] +
+                "\n\n【句集登場回数】" + person["句集登場回数"] + "回" +
+                "\n\n【備考】 " + person["備考"],
+                image= person["URL"],
+                url=""
+            )
+        )
+
+    st.deck_gl_chart(cards, num_cols=5)
+                              
+#     for index, person in df_selected.iterrows():
+#       card(
+#           title=person["俳名"],
+#           text="【本名/別名】 " + person["本名/別名"] +
+#           "\n\n【出身地】 " + person["出身地"] +
+#           "\n\n【職業】 " + person["職業_詳細"] +
+#           "\n\n【句集登場回数】" + person["句集登場回数"] + "回" +
+#           "\n\n【備考】 " + person["備考"],
+#           image= person["URL"],
+#           url=""
          
-      )
+#       )
 
 else:
     st.write("条件を入力してください")
