e6a4e6e425c21626ebeb2e867fc8fe9c14b0e4e2
Ryuji Kazuka <93454211+Kazuka7744jp@users.noreply.github.com>
2023-02-10 06:04:06 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -84,7 +84,7 @@ if haijin_input or location_input or job_input:
       )
 
 else:
-    st.write("条件を入力してください")
+    st.write("テキストボックスのいずれかに条件を入力してください")
     
 st.write('■データベース一覧')
 st.dataframe(df_haijin, width=None, height=500)
